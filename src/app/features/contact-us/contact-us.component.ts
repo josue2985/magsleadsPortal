9d5684d21e419ce10eb94b7ad892fac03d3093d4
Josue Brito <josue.brito.j@gmail.com>
2022-03-09 17:45:43 -0600

    added status for contact form

--- src/app/features/contact-us/contact-us.component.ts
+++ src/app/features/contact-us/contact-us.component.ts
@@ -14,7 +14,7 @@ export class ContactUsComponent implements OnInit {
 
   sectionTitle: string;
   status = 0;
-  confirmationUrl = environment.baseUrl + 'contactus?status=200';
+  // confirmationUrl = environment.baseUrl + 'contactus?status=200';
 
   contactForm = new FormGroup({
     name: new FormControl(''),
@@ -32,6 +32,7 @@ export class ContactUsComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    // console.log(this.confirmationUrl);
     this.sectionTitle = 'Crear un Magnet Lead';
 
     this.Activatedroute.queryParamMap
@@ -39,41 +40,45 @@ export class ContactUsComponent implements OnInit {
         this.status = +params.get('status') || 0;
     });
 
-    if (this.status === 200 && isPlatformBrowser(this.platformId)) {
+    if (this.status === 200 || this.status === 503 && isPlatformBrowser(this.platformId)) {
       const body = document.getElementById('body');
       body?.classList.add('no-scroll');
     }
   }
 
   onSubmit(): void {
-    const body = new HttpParams()
-    .set('form-name', 'contact')
-    .append('name', this.contactForm.value.name)
-    .append('company', this.contactForm.value.company)
-    .append('phone', this.contactForm.value.phone)
-    .append('email', this.contactForm.value.email)
-    .append('message', this.contactForm.value.message);
-    this.http.post('/', body.toString(), {headers: { 'Content-Type': 'application/x-www-form-urlencoded' }}).subscribe(
-      res => {},
-      err => {
-        if (err instanceof ErrorEvent) {
-          // client side error
-          alert('Mensaje Error.');
-          console.log(err.error.message);
-        } else {
-          // backend error. If status is 200, then the message successfully sent
-          if (err.status === 200) {
-            alert('Mensaje Enviado!');
+    if (isPlatformBrowser(this.platformId)) {
+      const body = new HttpParams()
+      .set('form-name', 'contact')
+      .append('name', this.contactForm.value.name)
+      .append('company', this.contactForm.value.company)
+      .append('phone', this.contactForm.value.phone)
+      .append('email', this.contactForm.value.email)
+      .append('message', this.contactForm.value.message);
+      this.http.post('/', body.toString(), {headers: { 'Content-Type': 'application/x-www-form-urlencoded' }}).subscribe(
+        res => {},
+        err => {
+          if (err instanceof ErrorEvent) {
+            // client side error
+            alert('Mensaje Error.');
+            console.log(err.error.message);
           } else {
-            alert('Backend Error.');
-            console.log('Error status:');
-            console.log(err.status);
-            console.log('Error body:');
-            console.log(err.error);
+            // backend error. If status is 200, then the message successfully sent
+            if (err.status === 200) {
+              alert('Mensaje Enviado!');
+              this.router.navigateByUrl('/contactus?status=200');
+            } else {
+              this.router.navigateByUrl('/contactus?status=503');
+              // console.log('Error status:');
+              // console.log(err.status);
+              // console.log('Error body:');
+              // console.log(err.error);
+            }
           }
         }
-      }
-    );
+      );
+     }
+
   }
 
   onDismiss(): void {
